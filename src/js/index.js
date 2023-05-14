86f3c2a26a48cfb5641a3cf730c7c077a6d178e1
João Victor Zaché <133550156+VictorZache@users.noreply.github.com>
2023-05-13 22:50:56 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+alert("Welcome to the project Pokedex");
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+const body = document.querySelector("body");
+
+botaoAlterarTema.addEventListener("click", () => {
+    
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+    body.classList.toggle("modo-escuro");
+    if (modoEscuroAtivo) { 
+         imagemBotaoTrocaDeTema.setAttribute("src", " ./src/imagens/sun.png");  
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", " ./src/imagens/moon.png");
+    }
+
+
+});
+
+
+
+
